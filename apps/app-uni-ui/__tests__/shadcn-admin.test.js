b4db698742a1ce0c16275d1c63195a2f393aa63d
pixelslee <pixelslee@foxmail.com>
2024-03-20 09:21:34 +0800

    文件结构调整

---
+++ apps/app-uni-ui/__tests__/shadcn-admin.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const shadcnAdmin = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(shadcnAdmin(), 'Hello from shadcnAdmin');
+console.info('shadcnAdmin tests passed');
