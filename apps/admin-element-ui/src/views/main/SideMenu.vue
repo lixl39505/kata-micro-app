4c4785b10e2398fe66c02aa77f2d6f6ba193fb26
katacoder <pixelslee@foxmail.com>
2024-04-25 12:55:56 +0800

    bugfix  element-ui SubMenu 递归调用报错，转函数组件

--- apps/admin-element-ui/src/views/main/SideMenu.vue
+++ apps/admin-element-ui/src/views/main/SideMenu.vue
@@ -1,29 +1,39 @@
-<template>
-  <ElSubmenu v-if="config.children.length" :index="config.path" :popperClass="popperClass">
+<template functional>
+  <ElSubmenu v-if="props.config.children.length" :index="props.config.path" :popperClass="props.popperClass">
     <template slot="title">
-      <SvgIcon v-if="config.meta.icon" :name="config.meta.icon" class="side-menu__icon"></SvgIcon>
-      <span>{{ config.meta.title }}</span>
+      <SvgIcon v-if="props.config.meta.icon" :name="props.config.meta.icon" class="side-menu__icon"></SvgIcon>
+      <span>{{ props.config.meta.title }}</span>
     </template>
-    <SideMenu v-for="item in config.children" :config="item" :key="item.path" :popperClass="popperClass"></SideMenu>
+    <SideMenu
+      v-for="item in props.config.children"
+      :config="item"
+      :key="item.path"
+      :popperClass="props.popperClass"
+    ></SideMenu>
   </ElSubmenu>
-  <ElMenuItem v-else :index="config.path">
-    <SvgIcon v-if="config.meta.icon" class="side-menu__icon" :name="config.meta.icon"></SvgIcon>
-    <span slot="title">{{ config.meta.title }}</span>
+  <ElMenuItem v-else :index="props.config.path">
+    <SvgIcon v-if="props.config.meta.icon" class="side-menu__icon" :name="props.config.meta.icon"></SvgIcon>
+    <span slot="title">{{ props.config.meta.title }}</span>
   </ElMenuItem>
 </template>
 <script lang="ts">
-export default {
-  name: 'SideMenu',
-}
-</script>
-<script lang="ts" setup>
+import Vue from 'vue'
 import { type MenuConfig } from '~/stores/user'
 
-defineProps<{
-  config: MenuConfig
-  popperClass?: string
-}>()
+const SideMenu = Vue.extend<{
+  props: {
+    config: MenuConfig
+    popperClass: string
+  }
+}>({
+  functional: true,
+})
+
+Vue.component('SideMenu', SideMenu)
+
+export default SideMenu
 </script>
+
 <style lang="scss">
 .side-menu {
   &__icon {
