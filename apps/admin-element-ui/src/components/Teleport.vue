25f0b6d3d00c29eca67351effb2b9303841d3338
katacoder <pixelslee@foxmail.com>
2024-04-29 17:03:21 +0800

    Vue2 Teleport dumb

--- apps/admin-element-ui/src/components/Teleport.vue
+++ apps/admin-element-ui/src/components/Teleport.vue
@@ -7,8 +7,15 @@ export default {
   },
   data() {
     let nonReactive = {
-      original: undefined as HTMLElement | undefined,
+      original: undefined,
+      dumb: undefined,
+      el: undefined,
       originalIndex: 0,
+    } as {
+      original?: HTMLElement
+      dumb?: Node
+      el?: Node
+      originalIndex: number
     }
     Object.preventExtensions(nonReactive)
     return nonReactive
@@ -18,28 +25,37 @@ export default {
     let el = children[0].elm
     let { original, originalIndex } = this.$data
     if (!el) return
-
+    this.el = el
     this.$watch(
       () => [this.to, this.disabled],
       () => {
         if (this.disabled) {
           // restore
-          if (original) original.insertBefore(original.childNodes[originalIndex], el)
+          original?.insertBefore(el, original.childNodes[originalIndex])
+          this.dumb?.parentNode?.removeChild(this.dumb)
           return
         } else {
           // preserve
           if (!original) {
             original = el?.parentElement || undefined
             if (original) originalIndex = Array.prototype.findIndex.call(original.childNodes, (v) => v === el)
-          } else return
+          }
+          if (!original) return
           let targetEl = typeof this.to === 'string' ? document.querySelector(this.to) : this.to
-          targetEl?.append(el)
+          if (!targetEl) return
+          // teleport
+          this.dumb = document.createComment(' teleport ')
+          original.insertBefore(this.dumb, original.childNodes[originalIndex])
+          targetEl.append(el)
         }
       },
       {
         immediate: true,
       },
     )
+  },
+  beforeDestroy() {
+    if (this.el) this.el.parentNode?.removeChild(this.el)
   },
   render(h) {
     let first = this.$slots.default?.[0]
