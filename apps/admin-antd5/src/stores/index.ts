8149a71fdc9f67b40bc5bb2ab2b3177a7f0d38d7
katacoder <pixelslee@foxmail.com>
2024-05-30 17:03:57 +0800

    rm 不符合 react 哲学的逻辑

--- apps/admin-antd5/src/stores/index.ts
+++ apps/admin-antd5/src/stores/index.ts
@@ -1,13 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { useDispatch, useSelector } from 'react-redux'
-import { ActionObserverMiddleware } from './ActionObserverMiddleware'
 import userReducer from './user/userSlice'
 
 const rootReducer = combineReducers({ user: userReducer })
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(ActionObserverMiddleware),
 })
 
 export type RootState = ReturnType<typeof rootReducer>
--- apps/admin-antd5/src/stores/user/withAuthGuard.tsx
+++
@@ -1,7 +0,0 @@
-// 登录验证路由组件
-export function withAuthGuard<P extends JSX.IntrinsicAttributes>(Component: React.ComponentType<P>): React.FC<P> {
-  return (props: P) => {
-    //
-    return <Component {...props} />
-  }
-}
