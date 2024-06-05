c5c62d06fcf176b3712ee3dabda48afb093dce89
katacoder <pixelslee@foxmail.com>
2024-06-05 15:40:06 +0800

    动态菜单

--- apps/admin-antd5/src/features/router/routerSlice.ts
+++ apps/admin-antd5/src/features/router/routerSlice.ts
@@ -1,74 +1,145 @@
-import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Action, createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { LazyRouteFunction, RouteObject } from 'react-router-dom'
+import type { WritableDraft } from 'immer'
 import { AppState } from '~/store'
 import App from '~/App'
 
-interface RouteInfo<Hanndle = { auth?: boolean }> {
-  componentName: string
-  path?: string
-  handle?: Hanndle
-  children?: RouteInfo<Hanndle>[]
+export interface Handle {
+  id: string
+  icon?: string
+  title?: string
+  lazy?: boolean
+  hideInMenu?: boolean
 }
-// 默认路由
-const initialState: RouteInfo[] = [
-  {
-    componentName: 'main/MainView',
-    children: [
-      {
-        path: '/',
-        componentName: 'Home',
-      },
-    ],
-  },
-  {
-    path: '/login',
-    handle: {
-      auth: false,
-    },
-    componentName: 'Login',
-  },
+export interface RouteInfo<H = Partial<Handle>> {
+  id: string
+  pid: string
+  path?: string
+  handle?: H
+  children?: RouteInfo<H>[]
+}
+export interface RouterState {
+  ids: string[]
+  record: Record<string, RouteInfo>
+}
+// 静态路由
+const staticalRoutes: RouteInfo[] = [
+  { id: 'MainView', pid: '', children: [] },
+  { id: 'Home', pid: 'MainView', path: '/', handle: { title: '首页' } },
+  { id: 'Login', pid: '', path: '/login', handle: { title: '登录' } },
 ]
+// helpers
+const u = {
+  createInitialState(list: RouteInfo[]) {
+    let result: RouterState = { ids: [], record: {} }
+    // normalize
+    list.forEach((v) => this.addRoute(result, v))
+    // nested
+    result.ids.forEach((id) => this.appendToParent(result, result.record[id]))
 
+    return result
+  },
+  addRoute(state: RouterState | WritableDraft<RouterState>, route: RouteInfo) {
+    let v = { ...route }
+    if (!v.handle) v.handle = {}
+    v.handle.id = route.id // 携带 id
+    state.ids.push(route.id)
+    state.record[route.id] = v
+  },
+  appendToParent(state: RouterState | WritableDraft<RouterState>, route: RouteInfo) {
+    if (route.pid) {
+      let parent = state.record[route.pid]
+      if (parent) {
+        if (parent.children) parent.children.push(route)
+        else parent.children = [route]
+      } else throw new Error(`Component <${route.pid}> doesn't exist`)
+    }
+  },
+  removeFromParent(state: RouterState | WritableDraft<RouterState>, route: RouteInfo) {
+    if (route.pid) {
+      let parent = state.record[route.pid]
+      if (parent && parent.children) {
+        let i = parent.children.findIndex((v) => v.id === route.id)
+        if (i >= 0) parent.children.splice(i, 1)
+      } else throw new Error(`Component <${route.pid}> doesn't exist`)
+    }
+  },
+}
+
+const initialState = u.createInitialState(staticalRoutes)
 const routerSlice = createSlice({
   name: 'router',
   initialState,
   reducers: {
     // 添加路由
-    addRoute(state, action: PayloadAction<RouteInfo>) {
-      let { payload: route } = action
-      state.push(route)
+    addRoutes(state, action: PayloadAction<RouteInfo | RouteInfo[]>) {
+      let routes = action.payload
+      if (!Array.isArray(routes)) routes = [routes]
+      routes.forEach((v) => {
+        if (state.ids.findIndex((id) => id === v.id) >= 0) return
+        u.addRoute(state, v)
+        u.appendToParent(state, v)
+      })
+    },
+    // 删除路由
+    removeRoutes(state, action: PayloadAction<RouteInfo | RouteInfo[]>) {
+      let routes = action.payload
+      if (!Array.isArray(routes)) routes = [routes]
+
+      routes.forEach((v) => {
+        let i = state.ids.findIndex((id) => id === v.id)
+        if (i < 0) return
+        state.ids.splice(i, 1)
+        delete state.record[v.id]
+        u.removeFromParent(state, v)
+      })
+    },
+    // 清空路由
+    clearRoutes(state) {
+      state.ids = []
+      state.record = {}
     },
   },
 })
 
-export interface RouterState {
-  // 最近访问的路由
-  routes: RouteObject[]
-}
 export default routerSlice.reducer
-export const { addRoute } = routerSlice.actions
-export const selectRouteInfos = (state: AppState) => state.router || null
-// 动态引入 route 组件
-let routeModules = import.meta.glob('~/views/**/*.route.tsx') as unknown as Record<
+export const { addRoutes, removeRoutes, clearRoutes } = routerSlice.actions
+export const selectAllRouteIds = (state: AppState) => state.router.ids
+export const selectRouteRecords = (state: AppState) => state.router.record
+export const selectAllRouteInfos = createSelector([selectAllRouteIds, selectRouteRecords], (ids, record) =>
+  ids.map((id) => record[id]),
+)
+export const selectRouteInfoTree = createSelector([selectAllRouteInfos], (routeInfos) =>
+  routeInfos.filter((v) => v.pid === ''),
+)
+
+// 静态路由组件
+let staticRouteComponents: Record<string, React.ComponentType> = {}
+
+// 懒加载路由模块
+let lazyRouteModules = import.meta.glob('~/views/**/*.lazy.tsx') as unknown as Record<
   string,
   LazyRouteFunction<RouteObject>
 >
-routeModules = Object.keys(routeModules).reduce<typeof routeModules>((acc, k) => {
-  let componentName = k.replace('.route.tsx', '')
-  componentName = componentName.split('/views/').pop()!
-  acc[componentName] = routeModules[k]
+lazyRouteModules = Object.keys(lazyRouteModules).reduce<typeof lazyRouteModules>((acc, k) => {
+  let id = k.replace('.lazy.tsx', '')
+  id = id.split('/views/').pop()!.split('/').pop()! // filename without .lazy.tsx
+  acc[id] = lazyRouteModules[k]
   return acc
 }, {})
-// 根据 routeInfo 配置动态生成 routes
-export const selectRoutes = createSelector([selectRouteInfos], (routeInfos) => {
+
+// 根据 routeInfos 动态生成 routes
+export const selectRoutes = createSelector([selectAllRouteInfos], (routeInfos) => {
   let routes: RouteObject[] = []
 
   function iterate(current: RouteInfo, parent?: RouteObject) {
     let route: RouteObject = {
-      lazy: routeModules[current.componentName],
       path: current.path,
       handle: current.handle,
     }
+    // 路由组件默认懒加载
+    if (current.handle?.lazy === false) route.Component = staticRouteComponents[current.id]
+    else route.lazy = lazyRouteModules[current.id]
 
     if (parent) {
       if (!parent.children) parent.children = []
@@ -84,6 +155,9 @@ export const selectRoutes = createSelector([selectRouteInfos], (routeInfos) => {
   return [
     {
       Component: App, // App 作为根布局
+      handle: {
+        id: 'App',
+      },
       children: routes,
     },
   ] as RouteObject[]
--- apps/admin-antd5/src/views/main/SideMenu.tsx
+++ apps/admin-antd5/src/views/main/SideMenu.tsx
@@ -1,46 +1,97 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { AppstoreOutlined, ContainerOutlined, DesktopOutlined, MailOutlined, PieChartOutlined } from '@ant-design/icons'
-import type { MenuProps } from 'antd'
 import { Menu } from 'antd'
+import type { MenuProps } from 'antd'
+import type { MenuItemType, SubMenuType } from 'antd/es/menu/hooks/useItems'
+import { Link, UIMatch, matchPath, useLocation, useMatch, useMatches } from 'react-router-dom'
+import { useAppSelector } from '~/store'
+import {
+  Handle,
+  RouteInfo,
+  selectAllRouteIds,
+  selectAllRouteInfos,
+  selectRouteInfoTree,
+  selectRouteRecords,
+} from '~/features/router/routerSlice'
+import AppIcon from '~/components/AppIcon'
 
-type MenuItem = Required<MenuProps>['items'][number]
+// type MenuItem = Required<MenuProps>['items'][number]
 
-const items: MenuItem[] = [
-  { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
-  { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
-  { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
-  {
-    key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
-    children: [
-      { key: '5', label: 'Option 5' },
-      { key: '6', label: 'Option 6' },
-      { key: '7', label: 'Option 7' },
-      { key: '8', label: 'Option 8' },
-    ],
-  },
-  {
-    key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
-    children: [
-      { key: '9', label: 'Option 9' },
-      { key: '10', label: 'Option 10' },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          { key: '11', label: 'Option 11' },
-          { key: '12', label: 'Option 12' },
-        ],
-      },
-    ],
-  },
-]
+// const items: MenuItem[] = [
+//   { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
+//   { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
+//   { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
+//   {
+//     key: 'sub1',
+//     label: 'Navigation One',
+//     icon: <MailOutlined />,
+//     children: [
+//       { key: '5', label: 'Option 5' },
+//       { key: '6', label: 'Option 6' },
+//       { key: '7', label: 'Option 7' },
+//       { key: '8', label: 'Option 8' },
+//     ],
+//   },
+//   {
+//     key: 'sub2',
+//     label: 'Navigation Two',
+//     icon: <AppstoreOutlined />,
+//     children: [
+//       { key: '9', label: 'Option 9' },
+//       { key: '10', label: 'Option 10' },
+//       {
+//         key: 'sub3',
+//         label: 'Submenu',
+//         children: [
+//           { key: '11', label: 'Option 11' },
+//           { key: '12', label: 'Option 12' },
+//         ],
+//       },
+//     ],
+//   },
+// ]
+
+// 兼容 Antd 的 MenuItem
+type MenuItem = { key: string; label: string | React.JSX.Element; icon?: React.JSX.Element; children?: MenuItem[] }
 
 const SideMenu: React.FC = () => {
-  return <Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline" theme="dark" items={items} />
+  const routeInfoTree = useAppSelector(selectRouteInfoTree)
+  const matches = useMatches()
+  const menus = useMemo(() => {
+    let items: MenuItem[] = [],
+      isMain = false
+
+    let walk = (cur: RouteInfo, parent?: MenuItem) => {
+      let t: MenuItem
+      if (cur.id === 'MainView') isMain = true
+      // skip layout route && out of MainView
+      if (cur.path && isMain) {
+        t = {
+          key: cur.id,
+          label: <Link to={cur.path}>{cur.handle?.title ?? ''}</Link>,
+        }
+
+        if (cur.handle?.icon) t.icon = <AppIcon name={cur.handle?.icon ?? ''} />
+
+        if (parent) {
+          if (!parent.children) parent.children = []
+          parent.children.push(t)
+        } else items.push(t)
+      }
+
+      if (cur.children) cur.children.forEach((v) => walk(v, t))
+      isMain = false
+    }
+
+    routeInfoTree.forEach((v) => walk(v))
+
+    return items
+  }, [routeInfoTree])
+
+  let loc = matches[matches.length - 1],
+    defaultKeys = [(loc?.handle as Handle).id]
+
+  return <Menu defaultSelectedKeys={defaultKeys} mode="inline" theme="dark" items={menus} />
 }
 
 export default SideMenu
