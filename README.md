1a48ec7f3222e04b0df9ff7aa1f3317385c8e2d0
katacoder <pixelslee@foxmail.com>
2024-06-18 23:38:49 +0800

    admin rc 子应用隐藏头部

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 简介
 
-SPA + 微前端，可用作练习或者项目模板。
+SPA + 微前端，可用于练习或者项目模板。
+
+```
+apps
+  |- admin-antd5
+  |- admin-element-plus
+  |- admin-element-ui
+```
--- apps/admin-antd5/src/views/main/MainView.lazy.tsx
+++ apps/admin-antd5/src/views/main/MainView.lazy.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { Button, Flex, Layout } from 'antd'
 // import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
@@ -16,45 +16,50 @@ export const Component = withUserAuth(() => {
   const { styles } = useStyles()
   const navigate = useNavigate()
   const [collapsed, setCollapsed] = useState(false)
+  const isSubApp = useRef(!!window?.__POWERED_BY_WUJIE__)
 
   return (
-    <Layout className={styles.main} hasSider>
+    <Layout className={styles.main} hasSider={!isSubApp.current}>
       {/* 左侧导航 */}
-      <Layout.Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className={styles.logo}>
-          <img src={antdSvg} width="28" height="28" style={{ verticalAlign: 'middle' }} />
-          {collapsed === false && <span style={{ marginLeft: '10px' }}>Ant Design</span>}
-        </div>
-        <SideMenu></SideMenu>
-      </Layout.Sider>
+      {!isSubApp.current && (
+        <Layout.Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className={styles.logo}>
+            <img src={antdSvg} width="28" height="28" style={{ verticalAlign: 'middle' }} />
+            {collapsed === false && <span style={{ marginLeft: '10px' }}>Ant Design</span>}
+          </div>
+          <SideMenu></SideMenu>
+        </Layout.Sider>
+      )}
       <Layout>
         {/* 顶部导航 */}
-        <Layout.Header>
-          <Flex align="center">
-            {/* 菜单折叠 */}
-            <Button
-              type="text"
-              size="large"
-              icon={collapsed ? <AppIcon name="MenuUnfoldOutlined" /> : <AppIcon name="MenuFoldOutlined" />}
-              onClick={() => setCollapsed(!collapsed)}
-            />
-            <Flex justify="flex-end" align="center" style={{ flex: 'auto' }}>
-              {/* 全屏 */}
+        {!isSubApp.current && (
+          <Layout.Header>
+            <Flex align="center">
+              {/* 菜单折叠 */}
               <Button
                 type="text"
                 size="large"
-                icon={<AppIcon name="fullscreen" />}
-                onClick={() => navigate('/login')}
+                icon={collapsed ? <AppIcon name="MenuUnfoldOutlined" /> : <AppIcon name="MenuFoldOutlined" />}
+                onClick={() => setCollapsed(!collapsed)}
               />
-              {/* 多语言切换 */}
-              <SelectLang />
-              {/* 当前用户 */}
-              <UserAction />
+              <Flex justify="flex-end" align="center" style={{ flex: 'auto' }}>
+                {/* 全屏 */}
+                <Button
+                  type="text"
+                  size="large"
+                  icon={<AppIcon name="fullscreen" />}
+                  onClick={() => navigate('/login')}
+                />
+                {/* 多语言切换 */}
+                <SelectLang />
+                {/* 当前用户 */}
+                <UserAction />
+              </Flex>
             </Flex>
-          </Flex>
-        </Layout.Header>
+          </Layout.Header>
+        )}
         {/* 历史标签 */}
-        <VisitedBar></VisitedBar>
+        {!isSubApp.current && <VisitedBar></VisitedBar>}
         {/* 内容 */}
         <Layout.Content className={styles.content}>
           <Content></Content>
